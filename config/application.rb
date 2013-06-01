e67f22188a0ed99d2f29c3de5e71412e6e7d4f61
paul mannion <mannion.paul@gmail.com>
2013-06-01 14:47:09 +0100

    precompile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module Usher3
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precomplie = true
-    config.assets.paths << "#{Rails.root}/app/public/videos"
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
   end
 end
